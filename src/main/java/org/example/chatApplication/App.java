28d4c469f9762589ef922d1d84ddb056b5e91efb
Snehal Shelte <sheltesnehal@gmail.com>
2024-10-10 16:15:27 +0530

    initial commit

--- src/main/java/org/example/chatApplication/App.java
+++ src/main/java/org/example/chatApplication/App.java
@@ -2,6 +2,6 @@ package org.example.chatApplication;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println("Hello world");
+        System.out.println("Hello world 123");
     }
 }
