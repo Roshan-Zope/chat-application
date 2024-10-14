63dfd68bf5b2c4256ce9fbf981d2fa16523a1f82
Snehal Shelte <sheltesnehal@gmail.com>
2024-10-15 01:29:47 +0530

    initial commit

--- src/main/java/org/example/chatApplication/view/HomeForm.java
+++ src/main/java/org/example/chatApplication/view/HomeForm.java
@@ -7,6 +7,8 @@ package org.example.chatApplication.view;
 import java.awt.*;
 import javax.swing.*;
 import javax.swing.GroupLayout;
+import javax.swing.border.*;
+import com.intellij.ide.ui.laf.*;
 
 /**
  * @author shelt
@@ -19,86 +21,235 @@ public class HomeForm extends JPanel {
     private void initComponents() {
         // JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
         // Generated using JFormDesigner Evaluation license - Snehal Shelte
-        panel1 = new JPanel();
+        leftPanel = new JPanel();
         label1 = new JLabel();
         usernameLbl = new JLabel();
         label2 = new JLabel();
-        panel2 = new JPanel();
-        panel3 = new JPanel();
+        firstChatLbl = new JLabel();
+        searchBarTF = new JTextField();
+        label3 = new JLabel();
+        label4 = new JLabel();
+        middlePanel = new JPanel();
+        label5 = new JLabel();
+        firstChatLbl2 = new JLabel();
+        label6 = new JLabel();
+        label7 = new JLabel();
+        label8 = new JLabel();
+        bottomPanel = new JPanel();
+        searchBarTF2 = new JTextField();
+        rightPanel = new JPanel();
+        searchBarTF3 = new JTextField();
+        label9 = new JLabel();
+        label10 = new JLabel();
+        label11 = new JLabel();
 
         //======== this ========
         setPreferredSize(new Dimension(900, 500));
-        setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new javax . swing. border .
-        EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmD\u0065sig\u006eer \u0045val\u0075ati\u006fn" , javax. swing .border . TitledBorder. CENTER ,javax . swing
-        . border .TitledBorder . BOTTOM, new java. awt .Font ( "Dia\u006cog", java .awt . Font. BOLD ,12 ) ,
-        java . awt. Color .red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .PropertyChangeListener ( )
-        { @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062ord\u0065r" .equals ( e. getPropertyName () ) )
-        throw new RuntimeException( ) ;} } );
+        setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new
+        javax.swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e",javax
+        .swing.border.TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java
+        .awt.Font("Dialo\u0067",java.awt.Font.BOLD,12),java.awt
+        .Color.red), getBorder())); addPropertyChangeListener(new java.beans.
+        PropertyChangeListener(){@Override public void propertyChange(java.beans.PropertyChangeEvent e){if("borde\u0072".
+        equals(e.getPropertyName()))throw new RuntimeException();}});
 
-        //======== panel1 ========
+        //======== leftPanel ========
         {
-            panel1.setPreferredSize(new Dimension(260, 500));
+            leftPanel.setPreferredSize(new Dimension(260, 500));
+            leftPanel.setBackground(new Color(0xeeeeee));
 
             //---- usernameLbl ----
             usernameLbl.setText("Snehal Shelte");
-            usernameLbl.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+            usernameLbl.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 19));
+            usernameLbl.setBorder(new MatteBorder(0, 0, 2, 0, Color.black));
 
             //---- label2 ----
-            label2.setText("text");
+            label2.setIcon(new ImageIcon(getClass().getResource("/Images/person_16dp.png")));
 
-            GroupLayout panel1Layout = new GroupLayout(panel1);
-            panel1.setLayout(panel1Layout);
-            panel1Layout.setHorizontalGroup(
-                panel1Layout.createParallelGroup()
-                    .addGroup(panel1Layout.createSequentialGroup()
-                        .addGap(225, 225, 225)
+            //---- firstChatLbl ----
+            firstChatLbl.setText("Roshan Zope");
+            firstChatLbl.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+
+            //---- label3 ----
+            label3.setText("text");
+
+            //---- label4 ----
+            label4.setText("text");
+
+            GroupLayout leftPanelLayout = new GroupLayout(leftPanel);
+            leftPanel.setLayout(leftPanelLayout);
+            leftPanelLayout.setHorizontalGroup(
+                leftPanelLayout.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                    .addGroup(GroupLayout.Alignment.LEADING, leftPanelLayout.createSequentialGroup()
+                        .addGroup(leftPanelLayout.createParallelGroup()
+                            .addGroup(leftPanelLayout.createSequentialGroup()
+                                .addGroup(leftPanelLayout.createParallelGroup()
+                                    .addGroup(leftPanelLayout.createSequentialGroup()
+                                        .addGap(15, 15, 15)
+                                        .addComponent(label2, GroupLayout.PREFERRED_SIZE, 54, GroupLayout.PREFERRED_SIZE))
+                                    .addGroup(leftPanelLayout.createSequentialGroup()
+                                        .addGap(23, 23, 23)
+                                        .addComponent(label3)))
+                                .addGap(12, 12, 12)
+                                .addGroup(leftPanelLayout.createParallelGroup()
+                                    .addGroup(leftPanelLayout.createSequentialGroup()
+                                        .addComponent(usernameLbl, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                                        .addComponent(label4)
+                                        .addGap(12, 12, 12))
+                                    .addGroup(leftPanelLayout.createSequentialGroup()
+                                        .addComponent(firstChatLbl)
+                                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+                            .addGroup(leftPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(searchBarTF, GroupLayout.PREFERRED_SIZE, 248, GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
                         .addComponent(label1, GroupLayout.PREFERRED_SIZE, 121, GroupLayout.PREFERRED_SIZE))
-                    .addGroup(panel1Layout.createSequentialGroup()
-                        .addGap(9, 9, 9)
-                        .addComponent(label2)
-                        .addGap(18, 18, 18)
-                        .addComponent(usernameLbl, GroupLayout.PREFERRED_SIZE, 117, GroupLayout.PREFERRED_SIZE))
             );
-            panel1Layout.setVerticalGroup(
-                panel1Layout.createParallelGroup()
-                    .addGroup(panel1Layout.createSequentialGroup()
-                        .addGap(19, 19, 19)
-                        .addGroup(panel1Layout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
-                            .addComponent(label2, GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE)
-                            .addComponent(usernameLbl, GroupLayout.DEFAULT_SIZE, 31, Short.MAX_VALUE))
+            leftPanelLayout.setVerticalGroup(
+                leftPanelLayout.createParallelGroup()
+                    .addGroup(leftPanelLayout.createSequentialGroup()
+                        .addGroup(leftPanelLayout.createParallelGroup()
+                            .addGroup(GroupLayout.Alignment.TRAILING, leftPanelLayout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(leftPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                    .addComponent(usernameLbl)
+                                    .addComponent(label4))
+                                .addGap(26, 26, 26))
+                            .addGroup(leftPanelLayout.createSequentialGroup()
+                                .addGap(31, 31, 31)
+                                .addComponent(label2, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18)))
+                        .addComponent(searchBarTF, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 17, Short.MAX_VALUE)
+                        .addGroup(leftPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(firstChatLbl)
+                            .addComponent(label3))
                         .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(label1)
-                        .addContainerGap(444, Short.MAX_VALUE))
+                        .addContainerGap(331, Short.MAX_VALUE))
             );
         }
 
-        //======== panel2 ========
+        //======== middlePanel ========
         {
 
-            GroupLayout panel2Layout = new GroupLayout(panel2);
-            panel2.setLayout(panel2Layout);
-            panel2Layout.setHorizontalGroup(
-                panel2Layout.createParallelGroup()
-                    .addGap(0, 399, Short.MAX_VALUE)
+            //---- label5 ----
+            label5.setText("text");
+
+            //---- firstChatLbl2 ----
+            firstChatLbl2.setText("Roshan Zope");
+            firstChatLbl2.setFont(new Font("Yu Gothic UI Semibold", Font.BOLD, 17));
+            firstChatLbl2.setBorder(new MatteBorder(0, 0, 1, 0, Color.black));
+
+            //---- label6 ----
+            label6.setText("text");
+
+            //---- label7 ----
+            label7.setText("text");
+
+            //---- label8 ----
+            label8.setText("text");
+
+            //======== bottomPanel ========
+            {
+
+                GroupLayout bottomPanelLayout = new GroupLayout(bottomPanel);
+                bottomPanel.setLayout(bottomPanelLayout);
+                bottomPanelLayout.setHorizontalGroup(
+                    bottomPanelLayout.createParallelGroup()
+                        .addGroup(GroupLayout.Alignment.TRAILING, bottomPanelLayout.createSequentialGroup()
+                            .addContainerGap(45, Short.MAX_VALUE)
+                            .addComponent(searchBarTF2, GroupLayout.PREFERRED_SIZE, 311, GroupLayout.PREFERRED_SIZE)
+                            .addGap(43, 43, 43))
+                );
+                bottomPanelLayout.setVerticalGroup(
+                    bottomPanelLayout.createParallelGroup()
+                        .addGroup(GroupLayout.Alignment.TRAILING, bottomPanelLayout.createSequentialGroup()
+                            .addContainerGap(20, Short.MAX_VALUE)
+                            .addComponent(searchBarTF2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                            .addGap(14, 14, 14))
+                );
+            }
+
+            GroupLayout middlePanelLayout = new GroupLayout(middlePanel);
+            middlePanel.setLayout(middlePanelLayout);
+            middlePanelLayout.setHorizontalGroup(
+                middlePanelLayout.createParallelGroup()
+                    .addGroup(middlePanelLayout.createSequentialGroup()
+                        .addGap(22, 22, 22)
+                        .addComponent(label5, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                        .addGap(30, 30, 30)
+                        .addComponent(firstChatLbl2, GroupLayout.PREFERRED_SIZE, 183, GroupLayout.PREFERRED_SIZE)
+                        .addGap(14, 14, 14)
+                        .addComponent(label6, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(label7, GroupLayout.PREFERRED_SIZE, 24, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(label8, GroupLayout.PREFERRED_SIZE, 21, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(19, Short.MAX_VALUE))
+                    .addComponent(bottomPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             );
-            panel2Layout.setVerticalGroup(
-                panel2Layout.createParallelGroup()
-                    .addGap(0, 500, Short.MAX_VALUE)
+            middlePanelLayout.setVerticalGroup(
+                middlePanelLayout.createParallelGroup()
+                    .addGroup(middlePanelLayout.createSequentialGroup()
+                        .addGap(23, 23, 23)
+                        .addGroup(middlePanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(label5, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(firstChatLbl2)
+                            .addComponent(label6)
+                            .addComponent(label8)
+                            .addComponent(label7))
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 367, Short.MAX_VALUE)
+                        .addComponent(bottomPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
             );
         }
 
-        //======== panel3 ========
+        //======== rightPanel ========
         {
+            rightPanel.setBackground(new Color(0xeeeeee));
 
-            GroupLayout panel3Layout = new GroupLayout(panel3);
-            panel3.setLayout(panel3Layout);
-            panel3Layout.setHorizontalGroup(
-                panel3Layout.createParallelGroup()
-                    .addGap(0, 223, Short.MAX_VALUE)
+            //---- label9 ----
+            label9.setText("IMG");
+            label9.setHorizontalAlignment(SwingConstants.CENTER);
+
+            //---- label10 ----
+            label10.setText("text");
+
+            //---- label11 ----
+            label11.setText("text");
+
+            GroupLayout rightPanelLayout = new GroupLayout(rightPanel);
+            rightPanel.setLayout(rightPanelLayout);
+            rightPanelLayout.setHorizontalGroup(
+                rightPanelLayout.createParallelGroup()
+                    .addGroup(GroupLayout.Alignment.TRAILING, rightPanelLayout.createSequentialGroup()
+                        .addContainerGap(68, Short.MAX_VALUE)
+                        .addComponent(label9, GroupLayout.PREFERRED_SIZE, 90, GroupLayout.PREFERRED_SIZE)
+                        .addGap(65, 65, 65))
+                    .addGroup(rightPanelLayout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addComponent(searchBarTF3, GroupLayout.PREFERRED_SIZE, 190, GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(19, Short.MAX_VALUE))
+                    .addGroup(rightPanelLayout.createSequentialGroup()
+                        .addGap(40, 40, 40)
+                        .addComponent(label10, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED, 66, Short.MAX_VALUE)
+                        .addComponent(label11, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                        .addGap(41, 41, 41))
             );
-            panel3Layout.setVerticalGroup(
-                panel3Layout.createParallelGroup()
-                    .addGap(0, 500, Short.MAX_VALUE)
+            rightPanelLayout.setVerticalGroup(
+                rightPanelLayout.createParallelGroup()
+                    .addGroup(rightPanelLayout.createSequentialGroup()
+                        .addGap(14, 14, 14)
+                        .addComponent(searchBarTF3, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+                        .addGap(42, 42, 42)
+                        .addComponent(label9, GroupLayout.PREFERRED_SIZE, 84, GroupLayout.PREFERRED_SIZE)
+                        .addGap(27, 27, 27)
+                        .addGroup(rightPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                            .addComponent(label10, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE)
+                            .addComponent(label11, GroupLayout.PREFERRED_SIZE, 38, GroupLayout.PREFERRED_SIZE))
+                        .addContainerGap(265, Short.MAX_VALUE))
             );
         }
 
@@ -108,28 +259,43 @@ public class HomeForm extends JPanel {
             layout.createParallelGroup()
                 .addGroup(layout.createSequentialGroup()
                     .addContainerGap()
-                    .addComponent(panel1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(leftPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(panel2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                    .addComponent(middlePanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                     .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
-                    .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(rightPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup()
-                .addComponent(panel1, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(panel2, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(panel3, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(leftPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(middlePanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(rightPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         // JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
     }
 
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
     // Generated using JFormDesigner Evaluation license - Snehal Shelte
-    private JPanel panel1;
+    private JPanel leftPanel;
     private JLabel label1;
     private JLabel usernameLbl;
     private JLabel label2;
-    private JPanel panel2;
-    private JPanel panel3;
+    private JLabel firstChatLbl;
+    private JTextField searchBarTF;
+    private JLabel label3;
+    private JLabel label4;
+    private JPanel middlePanel;
+    private JLabel label5;
+    private JLabel firstChatLbl2;
+    private JLabel label6;
+    private JLabel label7;
+    private JLabel label8;
+    private JPanel bottomPanel;
+    private JTextField searchBarTF2;
+    private JPanel rightPanel;
+    private JTextField searchBarTF3;
+    private JLabel label9;
+    private JLabel label10;
+    private JLabel label11;
     // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
